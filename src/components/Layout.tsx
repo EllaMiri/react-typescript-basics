fde92e3b050b8c4931e42a0866b17ba55ad8df68
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-02-14 16:59:51 +0100

    create initial files for layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Main from "./Main";
+import "./Layout.css";
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/components/Main.tsx
@@ -0,0 +1,40 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
+        dolores nihil laborum eaque voluptatem pariatur modi quisquam dolorum
+        similique quaerat reprehenderit, debitis ab deleniti ullam blanditiis at
+        repellat in accusantium!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
+        dolores nihil laborum eaque voluptatem pariatur modi quisquam dolorum
+        similique quaerat reprehenderit, debitis ab deleniti ullam blanditiis at
+        repellat in accusantium!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
+        dolores nihil laborum eaque voluptatem pariatur modi quisquam dolorum
+        similique quaerat reprehenderit, debitis ab deleniti ullam blanditiis at
+        repellat in accusantium!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
+        dolores nihil laborum eaque voluptatem pariatur modi quisquam dolorum
+        similique quaerat reprehenderit, debitis ab deleniti ullam blanditiis at
+        repellat in accusantium!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
+        dolores nihil laborum eaque voluptatem pariatur modi quisquam dolorum
+        similique quaerat reprehenderit, debitis ab deleniti ullam blanditiis at
+        repellat in accusantium!
+      </p>
+    </main>
+  );
+}
+
+export default Main;
